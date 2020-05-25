8afa1067d82d7a19ba8cc699a1fee9e94206940d
rvoges31 <59306580+rvoges31@users.noreply.github.com>
2020-05-25 14:22:44 +0800

    updated code comments for clarity

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 // Define baseline variables
 var len = 8;
@@ -8,19 +6,8 @@ var minLower = 1;
 var minNumber = 0;
 var minSpecial = 0;
 
-// Write password to the #password input
-function writePassword() {
- //var password = generatePassword();
- //var passwordText = document.querySelector("#password");
 
- //passwordText.value = password;
- console.log("Test 2");
-}
-
-
-
-
-// Trial from stack overflow with ipunt
+// password generating function
 function randomPassword(len, minUpper, minLower, minNumber, minSpecial) {
   var len = parseInt(prompt("Enter length from 8 to 128 characters."));
   while (len <8 ) {
@@ -43,22 +30,18 @@ function randomPassword(len, minUpper, minLower, minNumber, minSpecial) {
       A2Z = String.fromCharCode(...Array(91).keys()).slice(65),//A to Z uppercase
       a2z = String.fromCharCode(...Array(123).keys()).slice(97),//a to z lower case
       zero2nine = String.fromCharCode(...Array(58).keys()).slice(48),//0 to 9
-      specials = chars.replace(/\w/g, '');
-  if (minSpecial < 1) {chars = zero2nine + A2Z + a2z;}
-  if (minNumber < 1) {chars = chars.replace(zero2nine, '');}
-  let minRequired = minSpecial + minUpper + minLower + minNumber;
+      specials = chars.replace(/\w/g, ''); // Special characters only
+  if (minSpecial < 1) {chars = zero2nine + A2Z + a2z;} // re-wrties chars to not inlcude specials
+  if (minNumber < 1) {chars = chars.replace(zero2nine, '');} // re-writes chars to not inlucde numbers
+  let minRequired = minSpecial + minUpper + minLower + minNumber; //specifies minimum required variable to ensure the legnthe isnt shorter than len
   let rs = [].concat(
-      Array.from({length: minSpecial ? minSpecial : 0}, () => specials[Math.floor(Math.random() * specials.length)]),
-      Array.from({length: minUpper ? minUpper : 0}, () => A2Z[Math.floor(Math.random() * A2Z.length)]),
-      Array.from({length: minLower ? minLower : 0}, () => a2z[Math.floor(Math.random() * a2z.length)]),
-      Array.from({length: minNumber ? minNumber : 0}, () => zero2nine[Math.floor(Math.random() * zero2nine.length)]),
-      Array.from({length: Math.max(len, minRequired) - (minRequired ? minRequired : 0)}, () => chars[Math.floor(Math.random() * chars.length)]),
-  )
-  return rs.sort(() => Math.random() > Math.random()).join('')
+      Array.from({length: minSpecial ? minSpecial : 0}, () => specials[Math.floor(Math.random() * specials.length)]), // creates array of special charachters to inlcude
+      Array.from({length: minUpper ? minUpper : 0}, () => A2Z[Math.floor(Math.random() * A2Z.length)]), // creates array of uppercase characters to include
+      Array.from({length: minLower ? minLower : 0}, () => a2z[Math.floor(Math.random() * a2z.length)]), // creats array of lowercase characters to include
+      Array.from({length: minNumber ? minNumber : 0}, () => zero2nine[Math.floor(Math.random() * zero2nine.length)]), // creates array of numbers to include
+      Array.from({length: Math.max(len, minRequired) - (minRequired ? minRequired : 0)}, () => chars[Math.floor(Math.random() * chars.length)]), // creates array of *ALL characters to fill in remaining length
+  );
+  return rs.sort(() => Math.random() > Math.random()).join(''); // joins the array and randomises input
 }
-console.log(randomPassword(len, minUpper, minLower, minNumber, minSpecial)); // -> DDYxdVcvIyLgeB
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", randomPassword);
 
